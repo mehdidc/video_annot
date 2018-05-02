b9b1de23ad04760a2a526a937f9081625e37c227
mehdi cherti <mehdicherti@gmail.com>
2018-05-02 12:59:24 +0200

    drop down menu to select the class to annotate

--- annotator/urls.py
+++ annotator/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('class_selection', views.class_selection, name='class_selection'),
     path('video', views.random_video, name='random_video'),
     path('video/<int:video_id>', views.video, name='video'),
     path('nothing', views.nothing, name='nothing'),
--- annotator/views.py
+++ annotator/views.py
@@ -1,22 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect
 from django.shortcuts import render
-
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Video
 from .models import Label
+from .models import LabelType
 
 
 def index(request):
- return render(request, 'annotator/index.html')
+    return render(request, 'annotator/index.html')
 
 
 def nothing(request):
- return render(request, 'annotator/nothing.html')
+    return render(request, 'annotator/nothing.html')
+
+
+@login_required
+def class_selection(request):
+    classes = []
+    for label_type in LabelType.objects.filter():
+        name = label_type.name.replace('_', ' ')
+        slug = label_type.name
+        classes.append((name, slug))
+    context = {'classes': classes}
+    return render(request, 'annotator/class_selection.html', context)
 
 
 @login_required
@@ -25,15 +32,26 @@ def random_video(request):
         _insert_annotation_if_post_request(request)
     except AlreadyAnnotatedError:
         return render(request, 'annotator/annotated.html')
+
+    class_name = request.GET.get('class')
+    if class_name != '':
+        label_type_id = LabelType.objects.filter(name=class_name).first().id
+        where = ' AND v.query_label_type_id={}'.format(label_type_id)
+    else:
+        where = ''
     try:
         # return one non-annotated video randomly
-        video = Video.objects.raw(
+        req = (
             "SELECT * FROM annotator_video as v WHERE v.id NOT IN "
-            "(SELECT l.video_id FROM annotator_label as l, auth_user as u WHERE "
-            "u.id=l.user_id AND u.username='{}') ORDER BY RANDOM() LIMIT 1".format(request.user))[0]
+            "(SELECT l.video_id FROM annotator_label as l, auth_user as u "
+            "WHERE u.id=l.user_id AND u.username='{}') {} ORDER BY RANDOM() "
+            "LIMIT 1"
+        )
+        req = req.format(request.user, where)
+        videos = Video.objects.raw(req)
+        video = videos[0]
     except IndexError:
         return redirect('nothing')
-    
     context = {'video': video}
     return render(request, 'annotator/video.html', context)
 
@@ -58,7 +76,8 @@ def _insert_annotation_if_post_request(request):
         video_id = int(request.POST.get('video_id'))
         video = Video.objects.get(id=video_id)
         if answer not in ('Yes', 'No'):
-            raise ValueError('Issue in the form: "answer" should be either "Yes" or "No"')
+            raise ValueError(
+                'Issue in the form: "answer" should be either "Yes" or "No"')
         answer = True if answer == 'Yes' else False
         nb = Label.objects.filter(user=request.user, video=video).count()
         if nb > 0:
